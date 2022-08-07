db2ed909299a04600dfea596b73bdc064581dc75
Danielzxccc <araojodaniel321@gmail.com>
2022-08-07 15:55:52 +0800

    start scripts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const rwClient = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
 const getRandomHugot = require("./getRandomHugot");
 
-
+console.log("BOT STARTED")
 const tweet = async () => {
   try {
     await rwClient.v2.tweet(await getRandomHugot());
